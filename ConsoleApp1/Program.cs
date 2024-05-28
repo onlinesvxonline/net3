4d2c697df8573880a096dc3b35e4403c0eadce4a
onlinesvxonline <153940673+onlinesvxonline@users.noreply.github.com>
2024-05-28 15:59:39 +0300

    com3
    com3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            Server("Server", cancellationToken);
+
+            Console.WriteLine("Нажмите любую клавишу для завершения работы сервера...");
+            Console.ReadKey();
+
+            cancellationTokenSource.Cancel();
+        }
+
+        public static void Server(string name, CancellationToken cancellationToken)
+        {
+            UdpClient udpClient = new UdpClient(12345);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Сервер ждет сообщение от клиента");
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                var messageText = Encoding.UTF8.GetString(buffer);
+
+                if (messageText.ToLower() == "exit")
+                {
+                    Console.WriteLine("Сервер завершает работу...");
+                    break;
+                }
+
+                ThreadPool.QueueUserWorkItem(obj =>
+                {
+                    Message message = Message.DeserializeFromJson(messageText);
+                    message.Print();
+
+                    byte[] confirmationData = Encoding.UTF8.GetBytes("Сообщение успешно получено");
+                    udpClient.Send(confirmationData, confirmationData.Length, iPEndPoint);
+                });
+            }
+
+            udpClient.Close();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp1;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                SentMessage("Denis", i);
+            }
+            Console.ReadLine();
+        }
+
+        public static void SentMessage(string From, int i, string ip = "127.0.0.1")
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
+
+            while (true)
+            {
+                string messageText = "";
+
+                Console.Clear();
+                Console.WriteLine("Введите сообщение (для выхода введите 'exit'):");
+                messageText = Console.ReadLine();
+
+                if (messageText.ToLower() == "exit")
+                {
+                    Console.WriteLine("Выход из чата...");
+                    byte[] exitData = Encoding.UTF8.GetBytes("exit");
+                    udpClient.Send(exitData, exitData.Length, iPEndPoint);
+                    break;
+                }
+
+                Message message = new Message() { Text = messageText, NicknameFrom = From, NicknameTo = "Server", DateTime = DateTime.Now };
+                string json = message.SerializeMessageToJson();
+
+                byte[] data = Encoding.UTF8.GetBytes(json);
+                udpClient.Send(data, data.Length, iPEndPoint);
+
+                Console.WriteLine("Сообщение отправлено. Хотите отправить еще одно сообщение? (Y/N)");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() != "y") break;
+            }
+        }
+    }
+}
